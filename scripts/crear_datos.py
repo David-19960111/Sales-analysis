241eac46e27b458ec55200994954da52158495c5
David-19960111 <david11011996@gmail.com>
2025-01-10 10:10:26 -0300

    update db, visualizaciones and scripts

--- scripts/crear_datos.py
+++ scripts/crear_datos.py
@@ -4,15 +4,14 @@ import random
 
 faker = Faker()
 
-num_rows = 1000
+num_rows = 5236
 data = {
     "Fecha" : [faker.date_between(start_date = '-1y', end_date = 'today') for _ in range(num_rows)],
     "Producto" : [random.choice(["Laptop", "Smartphone", "Camiseta", "Zapatos", "Auriculares"]) for _ in range(num_rows)],
-    "Categoría": [random.choice(["Electrónica", "Ropa", "Accesorios"]) for _ in range(num_rows)],
     "Precio": [round(random.uniform(10, 500), 2) for _ in range(num_rows)],
     "Cantidad": [random.randint(1, 5) for _ in range(num_rows)],
-    "Ciudad": [random.choice(["Ciudad de México", "Guadalajara", "Monterrey", "Puebla", "Tijuana"]) for _ in range(num_rows)],
-    "Método de pago": [random.choice(["Tarjeta de crédito", "Efectivo", "Transferencia"]) for _ in range(num_rows)]
+    "Ciudad": [random.choice(["Buenos Aires", "Cordoba", "Tucuman", "Rio Negro", "San Luis", "Santa Fe", "Tierra del Fuego", "Salta", "Jujuy", "Neuquen"]) for _ in range(num_rows)],
+    "Metodo de pago": [random.choice(["Tarjeta de crédito", "Efectivo", "Transferencia"]) for _ in range(num_rows)]
 }
 
 df = pd.DataFrame(data)
